512a1c6b65fff289b33fc4daff479d8e3749fe2f
Johann-Peter Duchon <dejaypiii@gmail.com>
2023-11-01 13:28:47 +0100

    hanc_test: Add msvc build example

--- hanc_test/hanc_test.h
+++ hanc_test/hanc_test.h
@@ -11,10 +11,38 @@ typedef struct hanc_test_test {
 } hanc_test_test_t;
 
 #ifdef HANC_TEST
+
+#ifdef _MSC_VER // MSVC specifics
+
+#pragma section("hctsts$a", read) // Start of section
+#define HCTSTS_START                                                           \
+  _declspec(allocate("hctsts$a")) static hanc_test_test_t hctsts_start = {0};  \
+  return &hctsts_start + 1;       // skip the first empty reference
+#pragma section("hctsts$d", read) // Content/data of section
+#pragma section("hctsts$z", read) // End of section
+#define HCTSTS_END                                                             \
+  _declspec(allocate("hctsts$z")) static hanc_test_test_t hctsts_end = {0};    \
+  return &hctsts_end;
+#define MSVC_SECTION_HCTSTS __declspec(allocate("hctsts$d"))
+#define CLANG_SECTION_HCTSTS
+
+#else // Clang & GCC specifics
+
+#define MSVC_SECTION_HCTSTS
+#define CLANG_SECTION_HCTSTS __attribute__((used, section("hctsts")))
+#define HCTSTS_START                                                           \
+  extern hanc_test_test_t __start_hctsts;                                      \
+  return &__start_hctsts;
+#define HCTSTS_END                                                             \
+  extern hanc_test_test_t __stop_hctsts;                                       \
+  return &__stop_hctsts;
+
+#endif
+
 #define HC_TEST(test_name, content)                                            \
   void test_name(int *);                                                       \
-  static hanc_test_test_t hanc_test_##test_name                                \
-      __attribute__((used, section("hctsts"))) = {                             \
+  MSVC_SECTION_HCTSTS static hanc_test_test_t hanc_test_##test_name            \
+      CLANG_SECTION_HCTSTS = {                                                 \
           .test_func = test_name,                                              \
           .name = #test_name,                                                  \
   };                                                                           \
@@ -54,18 +82,16 @@ typedef struct hanc_test_test {
       return 0;                                                                \
   }                                                                            \
                                                                                \
-  hanc_test_test_t *get_hanc_tests_start(void) {                               \
-    extern hanc_test_test_t __start_hctsts;                                    \
-    return &__start_hctsts;                                                    \
-  }                                                                            \
-                                                                               \
-  hanc_test_test_t *get_hanc_tests_end(void) {                                 \
-    extern hanc_test_test_t __stop_hctsts;                                     \
-    return &__stop_hctsts;                                                     \
-  }
+  hanc_test_test_t *get_hanc_tests_start(void) { HCTSTS_START; }               \
+  hanc_test_test_t *get_hanc_tests_end(void) { HCTSTS_END; }
 #else
 #define HC_TEST(test_name, content)
 
-#define HANC_TEST_MAIN(content) int main(int argc, char **argv) content
+#define HANC_TEST_MAIN(content)                                                \
+  int main(int argc, char **argv) {                                            \
+    (void)argc;                                                                \
+    (void)argv;                                                                \
+    content                                                                    \
+  }
 #endif
 #endif
--- hanc_test/hanc_test_example.c
+++ hanc_test/hanc_test_example.c
@@ -4,8 +4,6 @@
 #include "hanc_test.h"
 
 HANC_TEST_MAIN({
-  (void)argc;
-  (void)argv;
   printf("Hello hanc\n");
   int result = add(7, 5);
   printf("Add 7+5: %d\n", result);
